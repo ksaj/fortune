5e4d462e3b4e37b15582f3c682b68b4aed612fac
Karsten Johansson <KarstenJohansson@gmail.com>
2020-01-18 21:00:59 -0500

    Spring cleaning

--- ksaj.sh
+++ ksaj.sh
@@ -4,7 +4,7 @@ strfile -c % ksaj ksaj.dat
 sudo cp ksaj* /usr/share/games/fortunes
 
 echo
-echo fortune -e ksaj
+echo \> fortune -e ksaj
 echo
 
 fortune -e ksaj
--- trump.sh
+++ trump.sh
@@ -4,7 +4,7 @@ strfile -c % trump trump.dat
 sudo cp trump* /usr/share/games/fortunes
 
 echo
-echo fortune -e trump
+echo \> fortune -e trump
 echo
 
 fortune -e trump
